ef68c8c42eaf1f7f6ae7424b336d21b2d495845d
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2021-09-26 04:18:26 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,141 @@
+// pdf chapter 14 t0 16 assement task 1
+// var nam= []
+// console.log(nam)
+
+
+// pdf chapter 14 t0 16 assement task 2
+// var names = ["Abdul","Rehman"]
+// console.log(names)
+
+
+// pdf chapter 14 t0 16 assement task 3
+// var num = ["1","3"];
+// console.log(num)
+
+
+// pdf chapter 14 t0 16 assement task 4
+// var num = [1,3];
+// console.log(num)
+
+
+// pdf chapter 14 t0 16 assement task 5
+// var mynam= [1,2,3,"Abdul","Rehman"]
+// console.log(mynam)
+
+
+// pdf chapter 14 t0 16 assement task 6
+// var boolen=[name#,$,%,]
+// alert(boolen)
+
+// pdf chapter 14 t0 16 assement task 7
+// document.write("<h1>Qualification</h1>");
+// var arr = ["SSC","HSC","BCS","BS","BCOM","MS","M. Phil.","Phd"];
+// arr.length
+// console.log(arr)
+
+
+// pdf chapter 14 t0 16 assement task 8
+// var score1 = ["Score of Michel","Score ofJhon","Score of Ton"]
+// var totalmark = 500;
+// var obtmark=prompt("Enter the your Matric Mark")
+// var score = obtmark/totalmark%100;
+// if(obtmark==421 && score){
+//     console.log(score1[0] + " " + obtmark+ " " +["is"] + " "+ " " +["percentage of Michal is  "] +" "+ score)
+// } 
+// else if (obtmark==399 && score){
+//     console.log(score1[1]+ " " + obtmark + " "+["is"] + " " + " " +["percentage of Jhon is "]+ " " + score)
+// }
+// else if(obtmark ==362 && score){
+//     console.log(score1[2]+ " " + obtmark + " "+["is"] + " " + " " +[ "percentage of ton is "] + " " + score)
+// }
+ 
+
+
+// pdf chapter 14 t0 16 assement task 9
+// var colors=["Blue","Red","Green","Black","white"];
+// document.write("The available colors in array are </br>");
+// for(var i=0;i<colors.length;i++){
+//     document.write(colors[i]);
+//     document.write("</br>")
+// }
+// var addcolor=prompt("Enter the color you want to add in the begining of array");
+// colors.unshift(addcolor);
+// document.write("The array after adding to start is </br>");
+// for(var i=0;i<colors.length;i++){
+//     document.write(colors[i]);
+//     document.write("</br>")
+// }
+// var addcolor=prompt("Enter the color you want to add at the end of array");
+// colors.push(addcolor);
+// document.write("The array after adding to end is </br>");
+// for(var i=0;i<colors.length;i++){
+//     document.write(colors[i]);
+//     document.write("</br>")
+// }
+// colors.unshift("Violet","Dark Green");
+// document.write("The array after adding two more colors in start is </br>");
+// for(var i=0;i<colors.length;i++){
+//     document.write(colors[i]);
+//     document.write("</br>")
+// }
+// colors.shift();
+// document.write("The array after deleting a color from start is </br>");
+// for(var i=0;i<colors.length;i++){
+//     document.write(colors[i]);
+//     document.write("</br>")
+// }
+// colors.pop();
+// document.write("The array after deleting color from end is </br>");
+// for(var i=0;i<colors.length;i++){
+//     document.write(colors[i]);
+//     document.write("</br>")
+// }
+
+// var index=prompt("Enter the index of array at which you want to add color");
+// addcolor=prompt("Enter the color you want to add at the disired index");
+// colors.splice(index,0,addcolor);
+// document.write("The array after adding color at given index is </br>");
+// for(var i=0;i<colors.length;i++){
+//     document.write(colors[i]);
+//     document.write("</br>")
+// }
+// index=prompt("Enter the index from which you start to delete colors");
+// var n=prompt("Enter the number of index you want to delete");
+// colors.splice(index,n);
+// document.write("The array after deleting from desired place is </br>");
+// for(var i=0;i<colors.length;i++){
+//     document.write(colors[i]);
+//     document.write("</br>")
+// }
+
+
+// var clean_list_Cities = ["Karachi","Lahore","Quetta ","Multan","Islamabad"]
+// if(clean_list_Cities="Karachi"){
+//     alert("Welcome to Karachi")
+// }
+
+// pdf assement chapter no 14 to 16 task this 12
+
+// var  arr =["This is my cat"];
+
+// console.log(arr)
+
+
+// pdf assement chapter no 14 to 16 task this 13
+// var arr=[];
+//     arr[0]="keyboard";
+//     arr[1]="mouse";
+//     arr[2]="printer";
+//     arr[3]="moniter";
+// document.write("Devices:"+arr  +" </br>");
+// document.write("Last in first out </br>");
+// document.write("out:</br>"+arr[3]  +" </br>");
+// document.write("out:</br>"+arr[2]  +" </br>");
+// document.write("out:</br>"+arr[1]  +" </br>");
+// document.write("out:</br>"+arr[0]  +" </br>");
+// document.write("First in first out </br>");
+// document.write("Last in first out </br>");
+// document.write("out:</br>"+arr[0]  +" </br>");
+// document.write("out:</br>"+arr[1]  +" </br>");
+// document.write("out:</br>"+arr[2]  +" </br>");
+// document.write("out:</br>"+arr[3]  +" </br>");
